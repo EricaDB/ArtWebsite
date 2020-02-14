0ba15d0d41e89180328c59dbc90d862fbdd20031
Erica Johnson <edj@uoregon.edu>
2020-02-14 12:33:55 -0800

    switch to using multiple pages for this web site instead of keeping it single page

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-function show(id) {
-	hideAll(id);
-	document.getElementById(id).style.display = 'block';
-}
-
-function hideAll(visibleId) {
-	var ids = ["home", "gallery"];
-	for (var i = 0; i < ids.length; i++) {
-		if (ids[i] !== visibleId) {
-			document.getElementById(ids[i]).style.display = 'none';
-		}
-	}
-}
+function aboutPage() {
+    location.href = "about.html";
+};
