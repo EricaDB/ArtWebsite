4c4275c9e05d23452a319412194d6dd78e38092a
Erica Johnson <edj@uoregon.edu>
2020-02-19 14:44:04 -0800

    in-progress work to add hover over thumbnails and viewing full-sized artwork in a modal screen

--- app.js
+++ app.js
@@ -5,3 +5,24 @@ function homePage() {
 function aboutPage() {
     location.href = "about.html";
 };
+
+function viewImage() {
+	// Get the modal
+	var modal = document.getElementById("myModal");
+	var modalImg = document.getElementById("img01");
+	var captionText = document.getElementById("caption");
+	modal.style.display = "block";
+	modalImg.src = this.src;
+	captionText.innerHTML = "Example Caption Text";
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	  modal.style.display = "none";
+	}
+}
+
+var img = document.getElementById("myImg");
+img.onclick = viewImage;
