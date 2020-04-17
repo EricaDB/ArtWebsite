3792ff04d0e44d7e39ffda265ef730666313924e
Erica Johnson <edj@uoregon.edu>
2020-04-17 13:30:21 -0700

    Add thumbnail hover text and darken

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ function aboutPage() {
     location.href = "about.html";
 };
 
+var fullImage = {
+	"witchesfamiliar": "https://ericadb-art-website.s3-us-west-2.amazonaws.com/Witchs_Familiar_thumbnail.jpg"
+}
+
 function viewImage() {
 	// Get the modal
 	var modal = document.getElementById("myModal");
 	var modalImg = document.getElementById("img01");
 	var captionText = document.getElementById("caption");
 	modal.style.display = "block";
-	modalImg.src = this.src;
+	modalImg.src = "https://ericadb-art-website.s3-us-west-2.amazonaws.com/Witchs_Familiar_thumbnail.jpg";
 	captionText.innerHTML = "Example Caption Text";
 
 	// Get the <span> element that closes the modal
