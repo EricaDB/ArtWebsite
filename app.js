af830c8ddaf7503257ef3d3c69d298ca290ab5cd
Erica Johnson <edj@uoregon.edu>
2020-04-17 17:04:10 -0700

    viewImage() now takes a parameter to specify which image and the modal now shows the full image instead of the thumbnail

--- app.js
+++ app.js
@@ -7,16 +7,23 @@ function aboutPage() {
 };
 
 var fullImage = {
-	"witchesfamiliar": "https://ericadb-art-website.s3-us-west-2.amazonaws.com/Witchs_Familiar_thumbnail.jpg"
+	"witchesFamiliar": "https://ericadb-art-website.s3-us-west-2.amazonaws.com/Witchs_Familiar.jpg",
+	"knight": "https://ericadb-art-website.s3-us-west-2.amazonaws.com/Knight.jpg",
+	"arcticRift": "https://ericadb-art-website.s3-us-west-2.amazonaws.com/Arctic_Rift.jpg",
+	"frozenFalls": "https://ericadb-art-website.s3-us-west-2.amazonaws.com/Frozen_Falls.jpg",
+	"xena": "https://ericadb-art-website.s3-us-west-2.amazonaws.com/Xena.JPG",
+	"theGreenFairy": "https://ericadb-art-website.s3-us-west-2.amazonaws.com/The_Green_Fairy.jpg",
+	"seasideWitch": "https://ericadb-art-website.s3-us-west-2.amazonaws.com/Seaside_Witch.jpg",
+	"selfPortrait": "https://ericadb-art-website.s3-us-west-2.amazonaws.com/Self_Portrait.JPG"
 }
 
-function viewImage() {
+function viewImage(imageId) {
 	// Get the modal
 	var modal = document.getElementById("myModal");
 	var modalImg = document.getElementById("img01");
 	var captionText = document.getElementById("caption");
 	modal.style.display = "block";
-	modalImg.src = "https://ericadb-art-website.s3-us-west-2.amazonaws.com/Witchs_Familiar_thumbnail.jpg";
+	modalImg.src = fullImage[imageId];
 	captionText.innerHTML = "Example Caption Text";
 
 	// Get the <span> element that closes the modal
