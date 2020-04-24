60c9016eaf1b0711645ffb9e5dd5de3410415da0
Erica Johnson <edj@uoregon.edu>
2020-04-24 14:12:51 -0700

    Make the modal sizing work for vertical and horizontal images

--- app.js
+++ app.js
@@ -18,15 +18,13 @@ var fullImage = {
 }
 
 function viewImage(imageId) {
-	// Get the modal
 	var modal = document.getElementById("myModal");
-	var modalImg = document.getElementById("img01");
+	var modalImg = document.getElementById("modalImg");
 	var captionText = document.getElementById("caption");
 	modal.style.display = "block";
 	modalImg.src = fullImage[imageId];
 	captionText.innerHTML = "Example Caption Text";
 
-	// Get the <span> element that closes the modal
 	var span = document.getElementsByClassName("close")[0];
 
 	// When the user clicks on <span> (x), close the modal
